fa40bc6083b2115280a50ab2ea898b26ecd694b2
Ghabriel <ghabriel.nunes@gmail.com>
2019-12-27 16:48:53 -0300

    Refactor attempt to use multiple RONs for the player actions

--- src/entities/player.rs
+++ src/entities/player.rs
@@ -1,16 +1,28 @@
 use amethyst::{
-    assets::Handle,
+    animation::{
+        Animation,
+        AnimationSet,
+        InterpolationFunction,
+        Sampler,
+        SpriteRenderChannel,
+        SpriteRenderPrimitive,
+    },
+    assets::{AssetStorage, Handle, Loader, ProgressCounter},
     core::Transform,
     ecs::{
         Component,
         DenseVecStorage,
         Entity,
+        Read,
+        ReadExpect,
         world::Builder,
         World,
         WorldExt,
     },
-    renderer::{SpriteRender, SpriteSheet},
+    renderer::{ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat},
 };
+
+use serde::{Deserialize, Serialize};
 
 pub struct Player {
     pub velocity: [f32; 2],
@@ -20,7 +32,30 @@ impl Component for Player {
     type Storage = DenseVecStorage<Self>;
 }
 
-pub fn initialise_player(world: &mut World, sprite_sheet: Handle<SpriteSheet>) -> Entity {
+fn load_sprite_sheet(world: &World, image_name: &str, ron_name: &str) -> Handle<SpriteSheet> {
+    let loader = world.read_resource::<Loader>();
+    let texture_handle = loader.load(image_name, ImageFormat::default(), (), &world.read_resource());
+
+    loader.load(ron_name, SpriteSheetFormat(texture_handle), (), &world.read_resource())
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
+pub enum AnimationId {
+    Walk,
+    Run,
+}
+
+pub struct PlayerSpriteSheets {
+    walking: Handle<SpriteSheet>,
+    running: Handle<SpriteSheet>,
+}
+
+pub fn initialise_player(world: &mut World) -> Entity {
+    let sprite_sheets = PlayerSpriteSheets {
+        walking: load_sprite_sheet(world, "sprites/player-walking.png", "sprites/player-walking.ron"),
+        running: load_sprite_sheet(world, "sprites/player-running.png", "sprites/player-running.ron"),
+    };
+
     let player = Player {
         velocity: [0., 0.],
     };
@@ -29,14 +64,62 @@ pub fn initialise_player(world: &mut World, sprite_sheet: Handle<SpriteSheet>) -
     transform.set_translation_xyz(200., 300., 0.);
 
     let sprite_render = SpriteRender {
-        sprite_sheet,
+        sprite_sheet: sprite_sheets.walking.clone(),
         sprite_number: 0,
     };
+
+    let mut animation_set = AnimationSet::new();
+    animation_set.insert(AnimationId::Walk, {
+        let mut progress_counter = ProgressCounter::new();
+
+        world.exec(|
+            (loader, sampler_storage, animation_storage): (
+                ReadExpect<Loader>,
+                Read<AssetStorage<Sampler<SpriteRenderPrimitive>>>,
+                Read<AssetStorage<Animation<SpriteRender>>>,
+            )
+        | {
+            let samplers: Vec<(usize, SpriteRenderChannel, Sampler<SpriteRenderPrimitive>)> = vec![
+                (
+                    0,
+                    SpriteRenderChannel::SpriteIndex,
+                    Sampler {
+                        input: vec![0.0, 0.25, 0.5, 0.75, 1.0],
+                        output: vec![
+                            SpriteRenderPrimitive::SpriteIndex(0),
+                            SpriteRenderPrimitive::SpriteIndex(1),
+                            SpriteRenderPrimitive::SpriteIndex(2),
+                            SpriteRenderPrimitive::SpriteIndex(3),
+                        ],
+                        function: InterpolationFunction::Step,
+                    }
+                )
+            ];
+
+            let animation = Animation::<SpriteRender> {
+                nodes: samplers
+                    .iter()
+                    .map(|(node_index, channel, sampler)| {
+                        (
+                            *node_index,
+                            channel.clone(),
+                            loader.load_from_data(sampler.clone(), &mut progress_counter, &sampler_storage),
+                        )
+                    })
+                    .collect(),
+            };
+
+            loader.load_from_data(animation, &mut progress_counter, &animation_storage)
+        })
+    });
+
+    world.insert(sprite_sheets);
 
     world
         .create_entity()
         .with(player)
         .with(transform)
         .with(sprite_render)
+        .with(animation_set)
         .build()
 }
--- src/states/overworld_state.rs
+++ src/states/overworld_state.rs
@@ -1,54 +1,32 @@
 use amethyst::{
     animation::{
-        AnimationBundle,
         AnimationCommand,
         AnimationControlSet,
         AnimationSet,
-        AnimationSetPrefab,
         ControlState,
         EndControl,
         get_animation_set,
     },
-    assets::{
-        Handle,
-        Loader,
-        PrefabData,
-        PrefabLoader,
-        PrefabLoaderSystemDesc,
-        ProgressCounter,
-        RonFormat,
-    },
-    core::{ArcThreadPool, bundle::SystemBundle, Transform},
-    derive::PrefabData,
+    core::{ArcThreadPool, Transform},
     ecs::{
         Dispatcher,
         DispatcherBuilder,
-        Entities,
-        Entity,
         Join,
-        ReadStorage,
         world::{Builder, EntitiesRes},
         World,
-        WriteStorage,
     },
-    Error,
     input::InputEvent,
     prelude::*,
     renderer::{
         Camera,
-        ImageFormat,
-        sprite::prefab::SpriteScenePrefab,
         SpriteRender,
-        SpriteSheet,
-        SpriteSheetFormat,
     },
 };
 
 use crate::{
-    entities::player::{Player, initialise_player},
+    entities::player::{AnimationId, Player, initialise_player},
 };
 
-use serde::{Deserialize, Serialize};
 use std::ops::Deref;
 
 pub fn initialise_camera(world: &mut World) {
@@ -62,40 +40,10 @@ pub fn initialise_camera(world: &mut World) {
         .build();
 }
 
-pub fn load_sprite_sheet(world: &World) -> Handle<SpriteSheet> {
-    let loader = world.read_resource::<Loader>();
-
-    let texture_handle = loader.load(
-        "sprites/player.png",
-        ImageFormat::default(),
-        (),
-        &world.read_resource(),
-    );
-
-    loader.load(
-        "sprites/player.ron",
-        SpriteSheetFormat(texture_handle),
-        (),
-        &world.read_resource(),
-    )
-}
-
-#[derive(Eq, PartialOrd, PartialEq, Hash, Debug, Copy, Clone, Deserialize, Serialize)]
-enum AnimationId {
-    Walk,
-    Run,
-}
-
-#[derive(Debug, Clone, Deserialize, PrefabData)]
-struct MyPrefabData {
-    sprite_scene: SpriteScenePrefab,
-    animation_set: AnimationSetPrefab<AnimationId, SpriteRender>,
-}
-
 #[derive(Default)]
 pub struct OverworldState<'a, 'b> {
     pub dispatcher: Option<Dispatcher<'a, 'b>>,
-    pub progress_counter: Option<ProgressCounter>,
+    // pub progress_counter: Option<ProgressCounter>,
 }
 
 impl SimpleState for OverworldState<'_, '_> {
@@ -103,38 +51,42 @@ impl SimpleState for OverworldState<'_, '_> {
         println!("Welcome to Pokémon Rust!");
 
         let mut dispatcher_builder = DispatcherBuilder::new()
-            .with(
-                PrefabLoaderSystemDesc::<MyPrefabData>::default().build(data.world),
-                "scene_loader",
-                &[],
-            )
+            // .with(
+            //     PrefabLoaderSystemDesc::<MyPrefabData>::default().build(data.world),
+            //     "scene_loader",
+            //     &[],
+            // )
             .with_pool(data.world.read_resource::<ArcThreadPool>().deref().clone());
 
-        AnimationBundle::<AnimationId, SpriteRender>::new(
-            "sprite_animation_control",
-            "sprite_sampler_interpolation",
-        ).build(data.world, &mut dispatcher_builder)
-            .expect("Failed to build AnimationBundle");
+        // AnimationBundle::<AnimationId, SpriteRender>::new(
+        //     "sprite_animation_control",
+        //     "sprite_sampler_interpolation",
+        // ).build(data.world, &mut dispatcher_builder)
+        //     .expect("Failed to build AnimationBundle");
 
         let mut dispatcher = dispatcher_builder.build();
         dispatcher.setup(data.world);
         self.dispatcher = Some(dispatcher);
 
-        let mut progress_counter = ProgressCounter::new();
-        let player_prefab = data.world.exec(|loader: PrefabLoader<'_, MyPrefabData>| {
-            loader.load(
-                "sprites/player.ron",
-                RonFormat,
-                &mut progress_counter,
-            )
-        });
-        // Creates new entities with components from MyPrefabData
-        data.world.create_entity().with(player_prefab).build();
-        self.progress_counter = Some(progress_counter);
+        // let mut progress_counter = ProgressCounter::new();
+        // let player_prefab = data.world.exec(|loader: PrefabLoader<'_, MyPrefabData>| {
+        //     loader.load(
+        //         "sprites/player-walking.ron",
+        //         RonFormat,
+        //         &mut progress_counter,
+        //     )
+        // });
+        // // Creates new entities with components from MyPrefabData
+        // data.world
+        //     .create_entity()
+        //     .with(player_prefab)
+        //     .build();
+        // self.progress_counter = Some(progress_counter);
 
-        // data.world.register::<Player>();
-        // let sprite_sheet = load_sprite_sheet(data.world);
-        // initialise_player(data.world, sprite_sheet.clone());
+        data.world.register::<Player>();
+        data.world.register::<AnimationSet<AnimationId, SpriteRender>>();
+        data.world.register::<AnimationControlSet<AnimationId, SpriteRender>>();
+        initialise_player(data.world);
         initialise_camera(data.world);
     }
 
@@ -145,33 +97,27 @@ impl SimpleState for OverworldState<'_, '_> {
             dispatcher.dispatch(world);
         }
 
-        if let Some(progress_counter) = &self.progress_counter {
-            if progress_counter.is_complete() {
-                let entities = world.read_resource::<EntitiesRes>();
-                let animation_sets = world.read_storage::<AnimationSet<AnimationId, SpriteRender>>();
-                let mut control_sets = world.write_storage::<AnimationControlSet<AnimationId, SpriteRender>>();
+        let entities = world.read_resource::<EntitiesRes>();
+        let animation_sets = world.read_storage::<AnimationSet<AnimationId, SpriteRender>>();
+        let mut control_sets = world.write_storage::<AnimationControlSet<AnimationId, SpriteRender>>();
 
-                for (entity, animation_set) in (&entities, &animation_sets).join() {
-                    get_animation_set(&mut control_sets, entity)
-                        .unwrap()
-                        .add_animation(
-                            AnimationId::Walk,
-                            &animation_set.get(&AnimationId::Walk).unwrap(),
-                            EndControl::Loop(None),
-                            1.0,
-                            AnimationCommand::Init,
-                        )
-                        .add_animation(
-                            AnimationId::Run,
-                            &animation_set.get(&AnimationId::Run).unwrap(),
-                            EndControl::Loop(None),
-                            1.0,
-                            AnimationCommand::Init,
-                        );
-                }
-
-                self.progress_counter = None;
-            }
+        for (entity, animation_set) in (&entities, &animation_sets).join() {
+            get_animation_set(&mut control_sets, entity)
+                .unwrap()
+                .add_animation(
+                    AnimationId::Walk,
+                    &animation_set.get(&AnimationId::Walk).unwrap(),
+                    EndControl::Loop(None),
+                    1.0,
+                    AnimationCommand::Init,
+                );
+                // .add_animation(
+                //     AnimationId::Run,
+                //     &animation_set.get(&AnimationId::Run).unwrap(),
+                //     EndControl::Loop(None),
+                //     1.0,
+                //     AnimationCommand::Init,
+                // );
         }
 
         Trans::None
@@ -186,34 +132,51 @@ impl SimpleState for OverworldState<'_, '_> {
                     let mut control_sets = data.world.write_storage::<AnimationControlSet<AnimationId, SpriteRender>>();
 
                     for (_, _, control_set) in (&entities, &animation_sets, &mut control_sets).join() {
-                        control_set.pause(AnimationId::Walk);
+                        // control_set.pause(AnimationId::Walk);
 
-                        control_set.animations
+                        let (_, animation) = control_set.animations
                             .iter_mut()
-                            .filter(|a| a.0 == AnimationId::Run)
-                            .for_each(|a| {
-                                a.1.state = ControlState::Requested;
-                                a.1.command = AnimationCommand::Start;
-                            });
+                            .find(|(id, _)| *id == AnimationId::Walk)
+                            .unwrap();
+                        animation.state = ControlState::Requested;
+                        animation.command = AnimationCommand::Start;
+
+                        // control_set.animations
+                        //     .iter_mut()
+                        //     .find(|(id, _)| *id == AnimationId::Run)
+                        //     .map(|(_, animation)| {
+                        //         animation.state = ControlState::Requested;
+                        //         animation.command = AnimationCommand::Start;
+                        //         animation
+                        //     })
+                        //     .unwrap();
                     }
                 },
-                InputEvent::ActionReleased(action) if action == "action" => {
+                // InputEvent::ActionReleased(action) if action == "action" => {
+                //     let entities = data.world.read_resource::<EntitiesRes>();
+                //     let animation_sets = data.world.read_storage::<AnimationSet<AnimationId, SpriteRender>>();
+                //     let mut control_sets = data.world.write_storage::<AnimationControlSet<AnimationId, SpriteRender>>();
+
+                //     for (_, _, control_set) in (&entities, &animation_sets, &mut control_sets).join() {
+                //         control_set.pause(AnimationId::Run);
+
+                //         let (_, animation) = control_set.animations
+                //             .iter_mut()
+                //             .find(|(id, _)| *id == AnimationId::Walk)
+                //             .unwrap();
+                //         animation.state = ControlState::Requested;
+                //         animation.command = AnimationCommand::Start;
+                //     }
+                // },
+                InputEvent::ActionPressed(action) if action == "cancel" => {
                     let entities = data.world.read_resource::<EntitiesRes>();
                     let animation_sets = data.world.read_storage::<AnimationSet<AnimationId, SpriteRender>>();
                     let mut control_sets = data.world.write_storage::<AnimationControlSet<AnimationId, SpriteRender>>();
 
                     for (_, _, control_set) in (&entities, &animation_sets, &mut control_sets).join() {
-                        control_set.pause(AnimationId::Run);
-
-                        control_set.animations
-                            .iter_mut()
-                            .filter(|a| a.0 == AnimationId::Walk)
-                            .for_each(|a| {
-                                a.1.state = ControlState::Requested;
-                                a.1.command = AnimationCommand::Start;
-                            });
+                        println!("{:#?}", control_set);
                     }
-                },
+                }
                 _ => {},
             }
         }
