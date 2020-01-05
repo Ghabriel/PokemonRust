b0b3fb8e65226b3f3aac2d4935acd22d98da6b7e
Ghabriel <ghabriel.nunes@gmail.com>
2020-01-04 22:40:25 -0300

    Attempt to improve rendering performance (not compiling)

--- src/magic/renderer.rs
+++ src/magic/renderer.rs
@@ -1,4 +1,5 @@
 use amethyst::{
+    assets::AssetStorage,
     core::{
         Hidden,
         HiddenPropagate,
@@ -13,21 +14,42 @@ use amethyst::{
         prelude::BitSet,
         Join,
         Read,
+        ReadExpect,
         ReadStorage,
         System,
         Write,
         World,
+        WorldExt,
     },
     renderer::{
         Backend,
+        batch::{GroupIterator, OneLevelBatch, OrderedOneLevelBatch},
         bundle::{RenderOrder, RenderPlan, Target},
         camera::{ActiveCamera, Camera},
         Factory,
-        pass::{DrawFlat2DDesc, DrawFlat2DTransparentDesc},
+        pass::{DrawFlat2DTransparentDesc},
+        pipeline::{PipelineDescBuilder, PipelinesBuilder},
+        pod::SpriteArgs,
         RenderGroupDesc,
         RenderPlugin,
+        rendy::{
+            command::{QueueId, RenderPassEncoder},
+            graph::{
+                render::{PrepareResult, RenderGroup},
+                GraphContext, NodeBuffer, NodeImage,
+            },
+            hal::{
+                image::Layout::ShaderReadOnlyOptimal,
+                pass::Subpass,
+            },
+        },
+        resources::Tint,
         Sprite,
+        SpriteRender,
+        SpriteSheet,
         sprite_visibility::{SpriteVisibility, SpriteVisibilitySortingSystem},
+        submodules::{DynamicVertexBuffer, FlatEnvironmentSub, TextureId, TextureSub},
+        Texture,
         transparent::Transparent,
     },
 };
@@ -35,6 +57,9 @@ use amethyst::{
 use crate::entities::map::Tile;
 
 use std::cmp::Ordering;
+
+#[cfg(feature = "profiler")]
+use thread_profiler::profile_scope;
 
 #[derive(Default, Debug)]
 pub struct PokeRenderer {
@@ -74,7 +99,7 @@ impl<B: Backend> RenderPlugin<B> for PokeRenderer {
         _world: &World,
     ) -> Result<(), Error> {
         plan.extend_target(self.target, |ctx| {
-            ctx.add(RenderOrder::Opaque, DrawFlat2DDesc::new().builder())?;
+            ctx.add(RenderOrder::Opaque, PokeDrawFlat2DDesc::new().builder())?;
             ctx.add(
                 RenderOrder::Transparent,
                 DrawFlat2DTransparentDesc::new().builder(),
@@ -106,6 +131,259 @@ impl<B: Backend> RenderPlugin<B> for PokeRenderer {
 
 //     panic!("No tiles");
 // }
+
+// ----------------------------------------------
+// TODO: move this to another file
+// ----------------------------------------------
+
+/// Draw opaque sprites without lighting.
+#[derive(Clone, Debug, Default, PartialEq)]
+pub struct PokeDrawFlat2DDesc;
+
+impl PokeDrawFlat2DDesc {
+    /// Create instance of `DrawFlat2D` render group
+    pub fn new() -> Self {
+        Default::default()
+    }
+}
+
+impl<B: Backend> RenderGroupDesc<B, World> for PokeDrawFlat2DDesc {
+    fn build(
+        self,
+        _ctx: &GraphContext<B>,
+        factory: &mut Factory<B>,
+        _queue: QueueId,
+        _aux: &World,
+        framebuffer_width: u32,
+        framebuffer_height: u32,
+        subpass: Subpass<'_, B>,
+        _buffers: Vec<NodeBuffer>,
+        _images: Vec<NodeImage>,
+    ) -> Result<Box<dyn RenderGroup<B, World>>, failure::Error> {
+        #[cfg(feature = "profiler")]
+        profile_scope!("build");
+
+        let env = FlatEnvironmentSub::new(factory)?;
+        let textures = TextureSub::new(factory)?;
+        let vertex = DynamicVertexBuffer::new();
+
+        let (pipeline, pipeline_layout) = build_sprite_pipeline(
+            factory,
+            subpass,
+            framebuffer_width,
+            framebuffer_height,
+            false,
+            vec![env.raw_layout(), textures.raw_layout()],
+        )?;
+
+        Ok(Box::new(DrawFlat2D::<B> {
+            pipeline,
+            pipeline_layout,
+            env,
+            textures,
+            vertex,
+            sprites: Default::default(),
+        }))
+    }
+}
+
+/// Draws opaque 2D sprites to the screen without lighting.
+#[derive(Debug)]
+pub struct DrawFlat2D<B: Backend> {
+    pipeline: B::GraphicsPipeline,
+    pipeline_layout: B::PipelineLayout,
+    env: FlatEnvironmentSub<B>,
+    textures: TextureSub<B>,
+    vertex: DynamicVertexBuffer<B, SpriteArgs>,
+    sprites: OneLevelBatch<TextureId, SpriteArgs>,
+}
+
+impl<B: Backend> RenderGroup<B, World> for DrawFlat2D<B> {
+    fn prepare(
+        &mut self,
+        factory: &Factory<B>,
+        _queue: QueueId,
+        index: usize,
+        _subpass: Subpass<'_, B>,
+        world: &World,
+    ) -> PrepareResult {
+        #[cfg(feature = "profiler")]
+        profile_scope!("prepare opaque");
+
+        let (
+            sprite_sheet_storage,
+            tex_storage,
+            visibility,
+            hiddens,
+            hidden_props,
+            sprite_renders,
+            transforms,
+            tints,
+        ) = <(
+            Read<'_, AssetStorage<SpriteSheet>>,
+            Read<'_, AssetStorage<Texture>>,
+            ReadExpect<'_, SpriteVisibility>,
+            ReadStorage<'_, Hidden>,
+            ReadStorage<'_, HiddenPropagate>,
+            ReadStorage<'_, SpriteRender>,
+            ReadStorage<'_, Transform>,
+            ReadStorage<'_, Tint>,
+        )>::fetch(world);
+
+        self.env.process(factory, index, world);
+
+        let sprites_ref = &mut self.sprites;
+        let textures_ref = &mut self.textures;
+
+        sprites_ref.clear_inner();
+
+        {
+            #[cfg(feature = "profiler")]
+            profile_scope!("gather_visibility");
+
+            (
+                &sprite_renders,
+                &transforms,
+                tints.maybe(),
+                &visibility.visible_unordered,
+            )
+                .join()
+                .filter_map(|(sprite_render, global, tint, _)| {
+                    let (batch_data, texture) = SpriteArgs::from_data(
+                        &tex_storage,
+                        &sprite_sheet_storage,
+                        &sprite_render,
+                        &global,
+                        tint,
+                    )?;
+                    let (tex_id, _) = textures_ref.insert(
+                        factory,
+                        world,
+                        texture,
+                        ShaderReadOnlyOptimal,
+                    )?;
+                    Some((tex_id, batch_data))
+                })
+                .for_each_group(|tex_id, batch_data| {
+                    sprites_ref.insert(tex_id, batch_data.drain(..))
+                });
+        }
+
+        self.textures.maintain(factory, world);
+
+        {
+            #[cfg(feature = "profiler")]
+            profile_scope!("write");
+
+            sprites_ref.prune();
+            self.vertex.write(
+                factory,
+                index,
+                self.sprites.count() as u64,
+                self.sprites.data(),
+            );
+        }
+
+        PrepareResult::DrawRecord
+    }
+
+    fn draw_inline(
+        &mut self,
+        mut encoder: RenderPassEncoder<'_, B>,
+        index: usize,
+        _subpass: Subpass<'_, B>,
+        _world: &World,
+    ) {
+        #[cfg(feature = "profiler")]
+        profile_scope!("draw opaque");
+
+        let layout = &self.pipeline_layout;
+        encoder.bind_graphics_pipeline(&self.pipeline);
+        self.env.bind(index, layout, 0, &mut encoder);
+        self.vertex.bind(index, 0, 0, &mut encoder);
+        for (&tex, range) in self.sprites.iter() {
+            if self.textures.loaded(tex) {
+                self.textures.bind(layout, 1, tex, &mut encoder);
+                unsafe {
+                    encoder.draw(0..4, range);
+                }
+            }
+        }
+    }
+
+    fn dispose(self: Box<Self>, factory: &mut Factory<B>, _world: &World) {
+        unsafe {
+            factory.device().destroy_graphics_pipeline(self.pipeline);
+            factory
+                .device()
+                .destroy_pipeline_layout(self.pipeline_layout);
+        }
+    }
+}
+
+// fn build_sprite_pipeline<B: Backend>(
+//     factory: &Factory<B>,
+//     subpass: hal::pass::Subpass<'_, B>,
+//     framebuffer_width: u32,
+//     framebuffer_height: u32,
+//     transparent: bool,
+//     layouts: Vec<&B::DescriptorSetLayout>,
+// ) -> Result<(B::GraphicsPipeline, B::PipelineLayout), failure::Error> {
+//     let pipeline_layout = unsafe {
+//         factory
+//             .device()
+//             .create_pipeline_layout(layouts, None as Option<(_, _)>)
+//     }?;
+
+//     let shader_vertex = unsafe { super::SPRITE_VERTEX.module(factory).unwrap() };
+//     let shader_fragment = unsafe { super::SPRITE_FRAGMENT.module(factory).unwrap() };
+
+//     let pipes = PipelinesBuilder::new()
+//         .with_pipeline(
+//             PipelineDescBuilder::new()
+//                 .with_vertex_desc(&[(SpriteArgs::vertex(), pso::VertexInputRate::Instance(1))])
+//                 .with_input_assembler(pso::InputAssemblerDesc::new(hal::Primitive::TriangleStrip))
+//                 .with_shaders(util::simple_shader_set(
+//                     &shader_vertex,
+//                     Some(&shader_fragment),
+//                 ))
+//                 .with_layout(&pipeline_layout)
+//                 .with_subpass(subpass)
+//                 .with_framebuffer_size(framebuffer_width, framebuffer_height)
+//                 .with_blend_targets(vec![pso::ColorBlendDesc {
+//                     mask: pso::ColorMask::ALL,
+//                     blend: if transparent {
+//                         Some(pso::BlendState::PREMULTIPLIED_ALPHA)
+//                     } else {
+//                         None
+//                     },
+//                 }])
+//                 .with_depth_test(pso::DepthTest {
+//                     fun: pso::Comparison::Less,
+//                     write: !transparent,
+//                 }),
+//         )
+//         .build(factory, None);
+
+//     unsafe {
+//         factory.destroy_shader_module(shader_vertex);
+//         factory.destroy_shader_module(shader_fragment);
+//     }
+
+//     match pipes {
+//         Err(e) => {
+//             unsafe {
+//                 factory.device().destroy_pipeline_layout(pipeline_layout);
+//             }
+//             Err(e)
+//         }
+//         Ok(mut pipes) => Ok((pipes.remove(0), pipeline_layout)),
+//     }
+// }
+
+// ----------------------------------------------
+// TODO: move this to another file
+// ----------------------------------------------
 
 #[derive(Default)]
 struct PokeVisibility {
@@ -157,6 +435,8 @@ impl<'a> System<'a> for PokeVisibility {
     ) {
         #[cfg(feature = "profiler")]
         profile_scope!("poke_visibility");
+
+        let a = std::time::Instant::now();
 
         let origin = Point3::origin();
 
@@ -226,6 +506,9 @@ impl<'a> System<'a> for PokeVisibility {
         visibility
             .visible_ordered
             .extend(self.transparent.iter().map(|c| c.entity));
+
+        let b = std::time::Instant::now();
+        println!("Delay: {}", (b - a).as_millis());
     }
 }
 
