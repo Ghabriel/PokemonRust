2866b5218237d10dd173bf0aa68d831756074c16
Ghabriel <ghabriel.nunes@gmail.com>
2020-01-06 09:24:09 -0300

    Add .tsx and .tmx files of the test map

---
+++ assets/Terrain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="Terrain" tilewidth="32" tileheight="32" spacing="2" tilecount="551" columns="29">
+ <image source="sprites/terrain.png" width="984" height="644"/>
+</tileset>
