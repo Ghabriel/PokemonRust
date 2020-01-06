5f9428eaf123a68d6fb8924d8188333a5914a213
Ghabriel <ghabriel.nunes@gmail.com>
2020-01-06 09:23:23 -0300

    Reorganize map code and its files, and handle layer 3

--- src/entities/map.rs
+++ src/entities/map.rs
@@ -11,9 +11,11 @@ use std::collections::HashMap;
 use super::load_sprite_sheet;
 
 pub struct Map {
-    terrains: Vec<Tile>,
+    // terrains: Vec<Tile>,
+    terrain_entity: Entity,
     solids: HashMap<MapPosition, Tile>,
-    decorations: HashMap<MapPosition, Tile>,
+    // decorations: HashMap<MapPosition, Tile>,
+    decoration_entity: Entity,
     actions: HashMap<MapPosition, GameAction>,
     scripts: Vec<MapScript>,
 }
@@ -72,30 +74,55 @@ pub enum MapScriptKind {
 }
 
 pub fn initialise_map(world: &mut World) -> Entity {
-    let num_tiles_x: u32 = 48;
-    let num_tiles_y: u32 = 48;
+    let terrain_entity = initialise_terrain_layer(world, "test_map");
+    let decoration_entity = initialise_decoration_layer(world, "test_map");
 
     let map = Map {
-        terrains: {
-            let mut vec = Vec::new();
-            vec.resize_with((num_tiles_x * num_tiles_y) as usize, Tile::default);
-            vec
-        },
+        // terrains: Vec::new(),
+        terrain_entity,
         solids: HashMap::new(),
-        decorations: HashMap::new(),
+        // decorations: HashMap::new(),
+        decoration_entity,
         actions: HashMap::new(),
         scripts: Vec::new(),
     };
 
-    let sprite_sheet = load_sprite_sheet(world, "test_map.png", "test_map.ron");
+    world
+        .create_entity()
+        .with(map)
+        .build()
+}
+
+fn initialise_terrain_layer(world: &mut World, map_name: &str) -> Entity {
+    let image_name = format!("maps/{}.png", map_name);
+    let ron_name = format!("maps/{}.ron", map_name);
 
     let sprite_render = SpriteRender {
-        sprite_sheet,
+        sprite_sheet: load_sprite_sheet(world, &image_name, &ron_name),
         sprite_number: 0,
     };
 
     let mut transform = Transform::default();
     transform.set_translation_xyz(0., 0., -1.);
+
+    world
+        .create_entity()
+        .with(transform)
+        .with(sprite_render)
+        .build()
+}
+
+fn initialise_decoration_layer(world: &mut World, map_name: &str) -> Entity {
+    let image_name = format!("maps/{}_layer3.png", map_name);
+    let ron_name = format!("maps/{}.ron", map_name);
+
+    let sprite_render = SpriteRender {
+        sprite_sheet: load_sprite_sheet(world, &image_name, &ron_name),
+        sprite_number: 0,
+    };
+
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(0., 0., 0.5);
 
     world
         .create_entity()
