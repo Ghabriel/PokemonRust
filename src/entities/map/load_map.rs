0280c38ffec26a483f68265b552d867b4485846b
Ghabriel <ghabriel.nunes@gmail.com>
2020-01-13 00:08:26 -0300

    Extract functions load_nearby_connections and get_new_map_bottom_left_corner

--- src/entities/map/load_map.rs
+++ src/entities/map/load_map.rs
@@ -22,6 +22,7 @@ use super::{
     map::{
         GameScript,
         Map,
+        MapConnection,
         MapScript,
         MapScriptKind,
         Tile,
@@ -42,74 +43,7 @@ pub fn initialise_map(world: &mut World) {
             .single_write(TextEvent::new("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."));
     }));
 
-    map.script_repository.push(GameScript::Native(|world| {
-        let (nearby_connections, bottom_left_corner) = {
-            let map = world.read_resource::<MapHandler>();
-            let players = world.read_storage::<Player>();
-            let transforms = world.read_storage::<Transform>();
-
-            let position = (&players, &transforms).join()
-                .map(|(_, transform)| transform.translation())
-                .next()
-                .unwrap();
-
-            let nearby_connections = map
-                .get_nearby_connections(&position)
-                .filter(|(_, connection)| !map.loaded_maps.contains_key(&connection.map))
-                .map(|(tile, connection)| (tile.clone(), connection.clone()))
-                .collect::<Vec<_>>();
-
-            let bottom_left_corner = map
-                .loaded_maps[&map.current_map]
-                .bottom_left_corner
-                .clone();
-
-            (nearby_connections, bottom_left_corner)
-        };
-
-        let loaded_maps: Vec<_> = nearby_connections
-            .iter()
-            .map(|(tile, connection)| {
-                let tile_size = TILE_SIZE as i32;
-                let half_tile = (TILE_SIZE / 2) as i32;
-                let tile_world_coordinates = Vector2::new(
-                    (tile.x as i32) * tile_size + half_tile + bottom_left_corner.x,
-                    (tile.y as i32) * tile_size + half_tile + bottom_left_corner.y,
-                );
-
-                // TODO: handle multi-connections (non-rectangular maps)
-                let (first_direction, external_tile) = connection.directions.iter().next().unwrap();
-
-                let external_tile_offset = match first_direction {
-                    Direction::Up => Vector2::new(0, tile_size),
-                    Direction::Down => Vector2::new(0, -tile_size),
-                    Direction::Left => Vector2::new(-tile_size, 0),
-                    Direction::Right => Vector2::new(tile_size, 0),
-                };
-
-                let external_tile_world_coordinates = tile_world_coordinates + external_tile_offset;
-                let external_left_corner = Vector3::new(
-                    external_tile_world_coordinates.x - half_tile - (external_tile.x as i32) * tile_size,
-                    external_tile_world_coordinates.y - half_tile - (external_tile.y as i32) * tile_size,
-                    0,
-                );
-
-                println!("Loading map {}...", connection.map);
-                println!("Connection tile (map coordinates): {:?}", tile);
-                println!("Connection tile (world coordinates): {:?}", tile_world_coordinates);
-                println!("External tile (world coordinates): {:?}", external_tile_world_coordinates);
-                println!("External left corner: {:?}", external_left_corner);
-
-                let map = load_map(world, &connection.map, Some(external_left_corner));
-                (connection.map.clone(), map)
-            })
-            .collect();
-
-        world
-            .write_resource::<MapHandler>()
-            .loaded_maps
-            .extend(loaded_maps);
-    }));
+    map.script_repository.push(GameScript::Native(load_nearby_connections));
 
     map.map_scripts.push(MapScript {
         when: MapScriptKind::OnTileChange,
@@ -124,6 +58,88 @@ pub fn initialise_map(world: &mut World) {
         },
         current_map: "test_map".to_string(),
     });
+}
+
+fn load_nearby_connections(world: &mut World) {
+    let (nearby_connections, bottom_left_corner) = {
+        let map = world.read_resource::<MapHandler>();
+        let players = world.read_storage::<Player>();
+        let transforms = world.read_storage::<Transform>();
+
+        let position = (&players, &transforms).join()
+            .map(|(_, transform)| transform.translation())
+            .next()
+            .unwrap();
+
+        let nearby_connections = map
+            .get_nearby_connections(&position)
+            .filter(|(_, connection)| !map.loaded_maps.contains_key(&connection.map))
+            .map(|(tile, connection)| (tile.clone(), connection.clone()))
+            .collect::<Vec<_>>();
+
+        let bottom_left_corner = map
+            .loaded_maps[&map.current_map]
+            .bottom_left_corner
+            .clone();
+
+        (nearby_connections, bottom_left_corner)
+    };
+
+    let loaded_maps: Vec<_> = nearby_connections
+        .iter()
+        .map(|(tile, connection)| {
+            let bottom_left_corner = get_new_map_bottom_left_corner(
+                &tile,
+                &connection,
+                &bottom_left_corner,
+            );
+            let map = load_map(world, &connection.map, Some(bottom_left_corner));
+            (connection.map.clone(), map)
+        })
+        .collect();
+
+    world
+        .write_resource::<MapHandler>()
+        .loaded_maps
+        .extend(loaded_maps);
+}
+
+fn get_new_map_bottom_left_corner(
+    tile: &Vector2<u32>,
+    connection: &MapConnection,
+    current_map_bottom_left_corner: &Vector3<i32>,
+) -> Vector3<i32> {
+    let tile_size = TILE_SIZE as i32;
+    let half_tile = (TILE_SIZE / 2) as i32;
+    let tile_world_coordinates = Vector2::new(
+        (tile.x as i32) * tile_size + half_tile + current_map_bottom_left_corner.x,
+        (tile.y as i32) * tile_size + half_tile + current_map_bottom_left_corner.y,
+    );
+
+    // TODO: handle multi-connections (non-rectangular maps)
+    let (first_direction, external_tile) = connection.directions.iter().next().unwrap();
+
+    let external_tile_offset = match first_direction {
+        Direction::Up => Vector2::new(0, tile_size),
+        Direction::Down => Vector2::new(0, -tile_size),
+        Direction::Left => Vector2::new(-tile_size, 0),
+        Direction::Right => Vector2::new(tile_size, 0),
+    };
+
+    let external_tile_world_coordinates = tile_world_coordinates + external_tile_offset;
+    let external_left_corner = Vector3::new(
+        external_tile_world_coordinates.x - half_tile - (external_tile.x as i32) * tile_size,
+        external_tile_world_coordinates.y - half_tile - (external_tile.y as i32) * tile_size,
+        0,
+    );
+
+    println!("Loading map {}...", connection.map);
+    println!("Connection tile (map coordinates): {:?}", tile);
+    println!("Connection tile (world coordinates): {:?}", tile_world_coordinates);
+    println!("External tile (world coordinates): {:?}", external_tile_world_coordinates);
+    println!("External left corner: {:?}", external_left_corner);
+
+    external_left_corner
 }
 
 pub fn load_map(world: &mut World, map_name: &str, bottom_left_corner: Option<Vector3<i32>>) -> Map {
