42d1431dca5d4b2b9d7a67c6a0cedc309f496163
Ghabriel <ghabriel.nunes@gmail.com>
2020-01-23 10:12:56 -0300

    Add Lua function rust_add_warp_event

--- pokemon_rust/src/lua/mod.rs
+++ pokemon_rust/src/lua/mod.rs
@@ -3,11 +3,17 @@ use amethyst::{
     utils::application_root_dir,
 };
 
-use crate::events::{EventQueue, TextEvent};
+use crate::{
+    entities::map::MapCoordinates,
+    events::{EventQueue, TextEvent, WarpEvent},
+};
 
 use rlua::{Context, Error, Function, Lua};
 
-use std::fs::read_to_string;
+use std::{
+    cell::RefCell,
+    fs::read_to_string,
+};
 
 thread_local! {
     static LUA: Lua = Lua::new();
@@ -48,15 +54,23 @@ fn run_with_native_functions<F, R>(world: &mut World, lua: &Lua, callback: F) ->
 where
     F: FnOnce(&Context) -> Result<R, Error>,
 {
+    let world = RefCell::new(world);
+
     lua.context(|context| {
         context.scope(|scope| {
             let rust_add_text_event = scope.create_function_mut(|_, text: String| {
-                add_text_event(world, text);
+                add_text_event(&mut world.borrow_mut(), text);
+                Ok(())
+            })?;
+
+            let rust_add_warp_event = scope.create_function_mut(|_, (map, x, y): (String, u32, u32)| {
+                add_warp_event(&mut world.borrow_mut(), map, x, y);
                 Ok(())
             })?;
 
             let globals = context.globals();
             globals.set("rust_add_text_event", rust_add_text_event)?;
+            globals.set("rust_add_warp_event", rust_add_warp_event)?;
 
             callback(&context)
         })
@@ -65,5 +79,10 @@ where
 
 fn add_text_event(world: &mut World, text: String) {
     let event = TextEvent::new(text, world);
+    world.write_resource::<EventQueue>().push(event);
+}
+
+fn add_warp_event(world: &mut World, map: String, x: u32, y: u32) {
+    let event = WarpEvent::new(map, MapCoordinates::new(x, y));
     world.write_resource::<EventQueue>().push(event);
 }
