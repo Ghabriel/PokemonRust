c6ad24d8aded6c8c80ef6868584ba7623fbb1651
Ghabriel <ghabriel.nunes@gmail.com>
2020-01-24 11:23:54 -0300

    Use Lua inheritance to reduce code duplication

--- pokemon_rust/lua/test.lua
+++ pokemon_rust/lua/test.lua
@@ -1,4 +1,18 @@
-TextEvent = {}
+Event = {}
+Event.__index = Event
+
+function Event:new()
+    local obj = { 0 }
+    setmetatable(obj, self)
+    return obj
+end
+
+function Event:dispatch()
+    rust_dispatch_event(self[1])
+end
+
+
+TextEvent = Event:new()
 TextEvent.__index = TextEvent
 
 function TextEvent:new(text)
@@ -7,12 +21,8 @@ function TextEvent:new(text)
     return obj
 end
 
-function TextEvent:dispatch()
-    rust_dispatch_event(self[1])
-end
 
-
-WarpEvent = {}
+WarpEvent = Event:new()
 WarpEvent.__index = WarpEvent
 
 function WarpEvent:new(map, x, y)
@@ -21,12 +31,8 @@ function WarpEvent:new(map, x, y)
     return obj
 end
 
-function WarpEvent:dispatch()
-    rust_dispatch_event(self[1])
-end
 
-
-ChainedEvents = {}
+ChainedEvents = Event:new()
 ChainedEvents.__index = ChainedEvents
 
 function ChainedEvents:new(events)
@@ -41,20 +47,6 @@ function ChainedEvents:new(events)
     return obj
 end
 
-function ChainedEvents:dispatch()
-    rust_dispatch_event(self[1])
-end
-
-
-function chained_events(events)
-    local chain = rust_create_chained_event()
-
-    for _, event in pairs(events) do
-        rust_add_event(chain, event[1])
-    end
-
-    rust_dispatch_event(chain)
-end
 
 function do_it()
     ChainedEvents:new({
