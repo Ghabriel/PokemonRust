144da46614670106dcdcf9c56c62937e7918e91c
Ghabriel <ghabriel.nunes@gmail.com>
2020-01-26 18:59:30 -0300

    Refactor map_parser to make it more robust

--- map_parser/src/main.rs
+++ map_parser/src/main.rs
@@ -1,13 +1,14 @@
-use regex::Regex;
+use regex::{Error as RegexError, Regex};
 
 use serde::Deserialize;
 
-use serde_xml_rs::from_reader;
+use serde_xml_rs::{Error as XmlError, from_reader};
 
 use std::{
     env,
+    fmt::{self, Display, Formatter},
     fs::{File, read_to_string},
-    io::Write,
+    io::{Error as IoError, Write},
     path::Path,
     process,
 };
@@ -32,6 +33,42 @@ struct TiledLayerData {
     body: String,
 }
 
+enum Error {
+    Io(IoError),
+    Regex(RegexError),
+    Parsing(&'static str),
+    Xml(XmlError),
+}
+
+impl From<IoError> for Error {
+    fn from(error: IoError) -> Error {
+        Error::Io(error)
+    }
+}
+
+impl From<RegexError> for Error {
+    fn from(error: RegexError) -> Error {
+        Error::Regex(error)
+    }
+}
+
+impl From<XmlError> for Error {
+    fn from(error: XmlError) -> Error {
+        Error::Xml(error)
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::Io(error) => write!(f, "{}", error),
+            Error::Regex(error) => write!(f, "{}", error),
+            Error::Parsing(error) => write!(f, "{}", error),
+            Error::Xml(error) => write!(f, "{}", error),
+        }
+    }
+}
+
 fn get_solid_list<'a>(map: &'a TiledMap) -> impl Iterator<Item = (usize, usize)> + 'a {
     map.layers
         .iter()
@@ -47,54 +84,74 @@ fn get_solid_list<'a>(map: &'a TiledMap) -> impl Iterator<Item = (usize, usize)>
         .map(move |(x, y)| (x, map.height - 1 - y))
 }
 
+fn update_map(map_folder_path: &Path) -> Result<(), Error> {
+    let map_ron_path = map_folder_path.join("map.ron");
+
+    let mut map_ron_content = read_to_string(&map_ron_path)?;
+
+    let solid_list_range = {
+        let regex = Regex::new(r"solids: \[[^\[]+\]")?;
+
+        match regex.find(&map_ron_content) {
+            Some(match_position) => {
+                let start = match_position.start();
+                let end = match_position.end();
+
+                start..end
+            },
+            None => {
+                return Err(Error::Parsing("Failed to find the list of solids in the map file."));
+            },
+        }
+    };
+
+    let map: TiledMap = {
+        let map_tmx_file = map_folder_path.join("map.tmx");
+        let file = File::open(map_tmx_file)?;
+        from_reader(file)?
+    };
+
+    map_ron_content.replace_range(
+        solid_list_range,
+        &format!(
+            "solids: [\n{}    ]",
+            get_solid_list(&map)
+                .map(|(x, y)| format!("        ({}, {}),\n", x, y))
+                .collect::<Vec<String>>()
+                .join(""),
+        ),
+    );
+
+    File::create(&map_ron_path)?
+        .write(map_ron_content.as_bytes())?;
+
+    Ok(())
+}
+
 fn main() {
     let mut args = env::args().skip(1);
 
     match args.next() {
         Some(map_folder) => {
             let map_folder_path = Path::new(&map_folder);
-            let map_ron_path = map_folder_path.join("map.ron");
 
-            let mut map_ron_content = read_to_string(&map_ron_path)
-                .expect("Failed to open ron file for reading");
+            let result = update_map(&map_folder_path);
 
-            let solid_list_range = {
-                let regex = Regex::new(r"solids: \[[^\[]+\]").unwrap();
-                let match_position = regex.find(&map_ron_content).unwrap();
-                let start = match_position.start();
-                let end = match_position.end();
+            match result {
+                Ok(_) => {
+                    let folder_name = map_folder_path
+                        .file_name()
+                        .expect("Invalid folder path")
+                        .to_str()
+                        .expect("Invalid folder name");
 
-                start..end
-            };
-
-            let map: TiledMap = {
-                let map_tmx_file = map_folder_path.join("map.tmx");
-                let file = File::open(map_tmx_file).expect("Failed to open map file");
-                from_reader(file).expect("Failed to deserialize map")
-            };
-
-            map_ron_content.replace_range(
-                solid_list_range,
-                &format!(
-                    "solids: [\n{}    ]",
-                    get_solid_list(&map)
-                        .map(|(x, y)| format!("        ({}, {}),\n", x, y))
-                        .collect::<Vec<String>>()
-                        .join(""),
-                ),
-            );
-
-            File::create(&map_ron_path)
-                .expect("Failed to open ron file for writing")
-                .write(map_ron_content.as_bytes())
-                .expect("Failed to write to ron file");
-
-            let folder_name = map_folder_path
-                .file_name()
-                .unwrap()
-                .to_str()
-                .unwrap();
-            println!("Updated map \"{}\".", folder_name);
+                    println!("Updated map \"{}\".", folder_name);
+                },
+                Err(err) => {
+                    eprintln!("An error occurred: {}", err);
+                    process::exit(2);
+                },
+            }
         },
         None => {
             println!("Usage: map_parser tiled_map.json");
