e317dbb2507047948ed3602d266bd2afaaa9654b
Ghabriel <ghabriel.nunes@gmail.com>
2020-02-24 22:44:20 -0300

    Implement basic battle functionality and a working Tackle

--- pokemon_rust/src/battle/backend.rs
+++ pokemon_rust/src/battle/backend.rs
@@ -1,14 +1,28 @@
-use crate::entities::pokemon::Pokemon;
+use crate::entities::{
+    character::CharacterId,
+    pokemon::{
+        get_all_moves,
+        movement::{Move, MoveCategory, MovePower},
+        Pokemon,
+        Stat,
+    },
+};
 
-use std::collections::VecDeque;
+use rand::{
+    distributions::{Distribution, Uniform},
+    seq::SliceRandom,
+    thread_rng,
+};
 
-use super::types::Battle;
+use std::collections::{HashMap, VecDeque};
+
+use super::types::{Battle, BattleType};
 
 /// Represents an event that can be sent from the frontend to the backend.
 #[derive(Debug)]
 pub struct FrontendEvent {
-    team: Team,
-    event: FrontendEventKind,
+    pub team: Team,
+    pub event: FrontendEventKind,
 }
 
 /// The kind of events that the frontend can send to the backend.
@@ -20,8 +34,9 @@ pub enum FrontendEventKind {
 /// The kind of events that the backend can send to the frontend.
 #[derive(Debug)]
 pub enum BattleEvent {
-    InitialSwitchIn(Team, Pokemon),
+    InitialSwitchIn(Team, usize),
     ChangeTurn(usize),
+    Damage { target: usize, amount: usize },
 }
 
 #[derive(Debug, Eq, PartialEq)]
@@ -30,18 +45,67 @@ pub enum Team {
     P2,
 }
 
+struct UsedMove<'a> {
+    user: usize,
+    target: usize,
+    movement: &'a Move,
+}
+
 pub struct BattleBackend {
-    data: Battle,
+    /// The type of battle that is happening.
+    battle_type: BattleType,
+    /// The current turn.
+    turn: usize,
+    /// The Pokémon that make up the first team. If the local player is
+    /// participating, this is always his team.
+    p1: TeamData,
+    /// The Pokémon that make up the second team.
+    p2: TeamData,
     input_events: VecDeque<FrontendEvent>,
     event_queue: Vec<BattleEvent>,
+    pokemon_repository: HashMap<usize, Pokemon>,
+}
+
+struct TeamData {
+    active_pokemon: Option<usize>,
+    party: VecDeque<usize>,
+    character_id: Option<CharacterId>,
 }
 
 impl BattleBackend {
     pub fn new(data: Battle) -> BattleBackend {
+        let mut pokemon_repository = HashMap::new();
+        let mut p1 = TeamData {
+            active_pokemon: None,
+            party: VecDeque::new(),
+            character_id: data.p1.character_id,
+        };
+        let mut p2 = TeamData {
+            active_pokemon: None,
+            party: VecDeque::new(),
+            character_id: data.p2.character_id,
+        };
+
+        for pokemon in data.p1.party.pokemon {
+            let index = pokemon_repository.len();
+            pokemon_repository.insert(index, pokemon);
+            p1.party.push_back(index);
+        }
+
+        for pokemon in data.p2.party.pokemon {
+            let index = pokemon_repository.len();
+            pokemon_repository.insert(index, pokemon);
+            p2.party.push_back(index);
+        }
+
         BattleBackend {
-            data,
+            battle_type: data.battle_type,
+            turn: 0,
+            p1,
+            p2,
             input_events: VecDeque::new(),
             event_queue: Vec::new(),
+            pokemon_repository,
         }
     }
 
@@ -50,21 +114,10 @@ impl BattleBackend {
     }
 
     pub fn tick(&mut self) -> impl Iterator<Item = BattleEvent> + '_ {
-        if self.data.turn == 0 {
+        if self.turn == 0 {
             self.first_tick();
-            self.next_turn();
-            return self.event_queue.drain(..);
-        }
-
-        let (p1_action, p2_action) = self.decompose_input_events();
-        let p1 = self.data.p1.active_pokemon.as_mut().unwrap();
-        let p2 = self.data.p2.active_pokemon.as_mut().unwrap();
-
-        match (p1_action, p2_action) {
-            (FrontendEventKind::UseMove(p1_index), FrontendEventKind::UseMove(p2_index)) => {
-                let p1_move = p1.moves[p1_index].as_ref().unwrap();
-                let p2_move = p1.moves[p1_index].as_ref().unwrap();
-            },
+        } else {
+            self.process_turn();
         }
 
         self.next_turn();
@@ -72,27 +125,115 @@ impl BattleBackend {
     }
 
     fn first_tick(&mut self) {
-        self.data.p1.active_pokemon = self.data.p1.party.pokemon.pop_front();
-        assert!(self.data.p1.active_pokemon.is_some());
+        self.p1.active_pokemon = self.p1.party.pop_front();
+        assert!(self.p1.active_pokemon.is_some());
         self.event_queue.push(BattleEvent::InitialSwitchIn(
             Team::P1,
-            self.data.p1.active_pokemon.as_ref().unwrap().clone(),
+            self.p1.active_pokemon.as_ref().unwrap().clone(),
         ));
 
-        self.data.p2.active_pokemon = self.data.p2.party.pokemon.pop_front();
-        assert!(self.data.p2.active_pokemon.is_some());
+        self.p2.active_pokemon = self.p2.party.pop_front();
+        assert!(self.p2.active_pokemon.is_some());
         self.event_queue.push(BattleEvent::InitialSwitchIn(
             Team::P2,
-            self.data.p2.active_pokemon.as_ref().unwrap().clone(),
+            self.p2.active_pokemon.as_ref().unwrap().clone(),
         ));
 
         // TODO: trigger things like Intimidate, entry hazards, Drought, etc
         // TODO: in which order?
     }
 
+    fn process_turn(&mut self) {
+        let (p1_action, p2_action) = self.decompose_input_events();
+
+        match (p1_action, p2_action) {
+            (FrontendEventKind::UseMove(p1_index), FrontendEventKind::UseMove(p2_index)) => {
+                let movedex = get_all_moves();
+                let p1 = self.p1.active_pokemon.unwrap();
+                let p2 = self.p2.active_pokemon.unwrap();
+
+                let p1_move = {
+                    let p1_move_id = self.pokemon_repository[&p1].moves[p1_index].as_ref().unwrap();
+                    let p1_move = movedex.get_move(&p1_move_id).unwrap();
+
+                    UsedMove {
+                        user: p1,
+                        target: p2,
+                        movement: &p1_move,
+                    }
+                };
+
+                let p2_move = {
+                    let p2_move_id = self.pokemon_repository[&p2].moves[p2_index].as_ref().unwrap();
+                    let p2_move = movedex.get_move(&p2_move_id).unwrap();
+
+                    UsedMove {
+                        user: p2,
+                        target: p1,
+                        movement: &p2_move,
+                    }
+                };
+
+                self.process_moves(vec![p1_move, p2_move].into_iter());
+            },
+        }
+    }
+
+    fn process_moves<'a>(
+        &mut self,
+        moves: impl Iterator<Item = UsedMove<'a>>,
+    ) {
+        let moves = self.sort_moves(moves);
+
+        for used_move in moves {
+            self.process_move(used_move);
+        }
+    }
+
+    fn sort_moves<'a>(
+        &mut self,
+        moves: impl Iterator<Item = UsedMove<'a>>,
+    ) -> impl Iterator<Item = UsedMove<'a>> {
+        let mut result: Vec<_> = moves.collect();
+
+        // Ensures random move order if both the priority and speed are equal
+        result.shuffle(&mut thread_rng());
+
+        result.sort_by(|a, b| {
+            b.movement.priority
+                .cmp(&a.movement.priority)
+                .then_with(|| {
+                    let a_speed = self.get_stat(a.user, Stat::Speed);
+                    let b_speed = self.get_stat(b.user, Stat::Speed);
+
+                    b_speed.cmp(&a_speed)
+                })
+        });
+
+        result.into_iter()
+    }
+
+    fn process_move(&mut self, used_move: UsedMove) {
+        match used_move.movement.category {
+            MoveCategory::Physical => {
+                let attack = self.get_stat(used_move.user, Stat::Attack);
+                let defense = self.get_stat(used_move.user, Stat::Defense);
+                self.inflict_damage(&used_move, attack, defense);
+            },
+            MoveCategory::Special => {
+                let attack = self.get_stat(used_move.user, Stat::SpecialAttack);
+                let defense = self.get_stat(used_move.user, Stat::SpecialDefense);
+                self.inflict_damage(&used_move, attack, defense);
+            },
+            MoveCategory::Status => {
+                // TODO
+            },
+        }
+    }
+
     fn next_turn(&mut self) {
-        self.data.turn += 1;
-        self.event_queue.push(BattleEvent::ChangeTurn(self.data.turn));
+        self.turn += 1;
+        self.event_queue.push(BattleEvent::ChangeTurn(self.turn));
     }
 
     fn decompose_input_events(&mut self) -> (FrontendEventKind, FrontendEventKind) {
@@ -108,6 +249,74 @@ impl BattleBackend {
             (Team::P1, Team::P2) => (first.event, second.event),
             (Team::P2, Team::P1) => (second.event, first.event),
             _ => panic!("Invalid input events: each event must be emitted by a different team."),
+        }
+    }
+}
+
+impl BattleBackend {
+    fn inflict_damage(&mut self, used_move: &UsedMove, attack: usize, defense: usize) {
+        let damage = self.get_move_damage(&used_move, attack, defense);
+
+        let target = self.pokemon_repository.get_mut(&used_move.target).unwrap();
+        target.current_hp = target.current_hp.saturating_sub(damage);
+
+        self.event_queue.push(BattleEvent::Damage {
+            target: used_move.target,
+            amount: damage,
+        });
+
+        // TODO: trigger effects like Static
+    }
+}
+
+impl BattleBackend {
+    fn get_stat(&self, pokemon: usize, stat: Stat) -> usize {
+        // TODO: take stat stages and other factors into account
+        self.pokemon_repository[&pokemon].stats[stat as usize]
+    }
+
+    fn get_move_power(&self, used_move: &UsedMove) -> usize {
+        let mov = used_move.movement;
+
+        match mov.power_modifier {
+            Some(modifier) => {
+                let user = &self.pokemon_repository[&used_move.user];
+                let target = &self.pokemon_repository[&used_move.target];
+                modifier(user, target, mov)
+            },
+            None => match mov.base_power {
+                MovePower::Constant(value) => value,
+                MovePower::Special => 0,
+            },
+        }
+    }
+
+    fn get_move_damage(&mut self, used_move: &UsedMove, attack: usize, defense: usize) -> usize {
+        let level = self.pokemon_repository[&used_move.user].level as f32;
+        let level_modifier = (2. * level) / 5. + 2.;
+        let power = self.get_move_power(&used_move) as f32;
+        let stat_ratio = (attack as f32) / (defense as f32);
+
+        let modifier = {
+            let targets = 1.; // TODO: handle multi-target moves
+            let weather = 1.; // TODO
+            let critical = 1.; // TODO
+            let random = Uniform::new(85., 100.).sample(&mut thread_rng()) / 100.;
+            let stab = 1.; // TODO
+            let effectiveness = 1.; // TODO
+            let burn = 1.; // TODO
+            let other = 1.; // TODO
+
+            targets * weather * critical * random * stab * effectiveness * burn * other
+        };
+
+        let damage = ((level_modifier * power * stat_ratio) / 50. + 2.) * modifier;
+        let damage = damage as usize;
+
+        if damage == 0 {
+            1
+        } else {
+            damage
         }
     }
 }
--- pokemon_rust/src/battle/tests.rs
+++ pokemon_rust/src/battle/tests.rs
@@ -1,6 +1,6 @@
 use crate::{
     battle::{
-        backend::{BattleBackend, BattleEvent, Team},
+        backend::{BattleBackend, BattleEvent, FrontendEvent, FrontendEventKind, Team},
         types::{Battle, BattleCharacterTeam, BattleType, Party},
     },
     entities::pokemon::{
@@ -23,7 +23,7 @@ fn switches_in_all_participants_in_first_turn() {
     match events.next().unwrap() {
         BattleEvent::InitialSwitchIn(team, pokemon) => {
             assert_eq!(team, Team::P1);
-            assert_eq!(pokemon.species_id, "Rattata");
+            assert_eq!(pokemon, 0);
         },
         _ => panic!("Wrong event kind"),
     }
@@ -31,7 +31,42 @@ fn switches_in_all_participants_in_first_turn() {
     match events.next().unwrap() {
         BattleEvent::InitialSwitchIn(team, pokemon) => {
             assert_eq!(team, Team::P2);
-            assert_eq!(pokemon.species_id, "Pidgey");
+            assert_eq!(pokemon, 1);
+        },
+        _ => panic!("Wrong event kind"),
+    }
+}
+
+#[test]
+fn tackle_deals_damage() {
+    let battle_type = BattleType::Single;
+    let p1 = simple_team("Rattata", 3);
+    let p2 = simple_team("Pidgey", 3);
+    let mut backend = BattleBackend::new(Battle::new(battle_type, p1, p2));
+
+    backend.tick();
+
+    backend.push_frontend_event(FrontendEvent {
+        team: Team::P1,
+        event: FrontendEventKind::UseMove(0),
+    });
+    backend.push_frontend_event(FrontendEvent {
+        team: Team::P2,
+        event: FrontendEventKind::UseMove(0),
+    });
+    let mut events = backend.tick();
+
+    let first = events.next().unwrap();
+    let second = events.next().unwrap();
+
+    match (first, second) {
+        (
+            BattleEvent::Damage { target: t1, amount: a1 },
+            BattleEvent::Damage { target: t2, amount: a2 },
+        ) => {
+            assert_ne!(t1, t2);
+            assert!(a1 >= 3 && a1 <= 5);
+            assert!(a2 >= 3 && a2 <= 5);
         },
         _ => panic!("Wrong event kind"),
     }
