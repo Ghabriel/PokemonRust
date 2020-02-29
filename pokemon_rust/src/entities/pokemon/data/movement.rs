822c4fa12eaf67db55212ac632db85293fcc66f3
Ghabriel <ghabriel.nunes@gmail.com>
2020-02-29 10:28:15 -0300

    Sort move data by name

--- pokemon_rust/src/entities/pokemon/data/movement.rs
+++ pokemon_rust/src/entities/pokemon/data/movement.rs
@@ -22,19 +22,43 @@ lazy_static! {
         let mut result = Vec::new();
 
         result.push(Move {
-            id: "Tackle".to_string(),
-            display_name: "Tackle".to_string(),
+            id: "Bite".to_string(),
+            display_name: "Bite".to_string(),
             description: "".to_string(), // TODO
-            move_type: PokemonType::Normal,
+            move_type: PokemonType::Dark,
             category: MoveCategory::Physical,
-            base_power: MovePower::Constant(40),
+            base_power: MovePower::Constant(60),
             power_modifier: None,
             accuracy: Some(100),
-            pp: 35,
+            pp: 25,
             priority: 0,
             target_type: TargetType::SingleAdjacentTarget,
             multi_hit: None,
+            // TODO: 30% flinch
             secondary_effect: None,
+            critical_hit: false,
+        });
+
+        result.push(Move {
+            id: "Growl".to_string(),
+            display_name: "Growl".to_string(),
+            description: "".to_string(), // TODO
+            move_type: PokemonType::Normal,
+            category: MoveCategory::Status,
+            base_power: MovePower::Special,
+            power_modifier: None,
+            accuracy: Some(100),
+            pp: 40,
+            priority: 0,
+            target_type: TargetType::AllAdjacentFoes,
+            multi_hit: None,
+            secondary_effect: Some(SecondaryEffect {
+                chance: 100,
+                effect: SimpleEffect::StatChange {
+                    changes: vec![(Stat::Attack, -1)],
+                    target: SimpleEffectTarget::MoveTarget,
+                }
+            }),
             critical_hit: false,
         });
 
@@ -56,21 +80,43 @@ lazy_static! {
         });
 
         result.push(Move {
-            id: "Bite".to_string(),
-            display_name: "Bite".to_string(),
+            id: "Harden".to_string(),
+            display_name: "Harden".to_string(),
             description: "".to_string(), // TODO
-            move_type: PokemonType::Dark,
+            move_type: PokemonType::Normal,
+            category: MoveCategory::Status,
+            base_power: MovePower::Special,
+            power_modifier: None,
+            accuracy: None,
+            pp: 30,
+            priority: 0,
+            target_type: TargetType::User,
+            multi_hit: None,
+            secondary_effect: Some(SecondaryEffect {
+                chance: 100,
+                effect: SimpleEffect::StatChange {
+                    changes: vec![(Stat::Defense, 1)],
+                    target: SimpleEffectTarget::MoveUser,
+                }
+            }),
+            critical_hit: false,
+        });
+
+        result.push(Move {
+            id: "KarateChop".to_string(),
+            display_name: "Karate Chop".to_string(),
+            description: "".to_string(), // TODO
+            move_type: PokemonType::Fight,
             category: MoveCategory::Physical,
-            base_power: MovePower::Constant(60),
+            base_power: MovePower::Constant(50),
             power_modifier: None,
             accuracy: Some(100),
             pp: 25,
             priority: 0,
             target_type: TargetType::SingleAdjacentTarget,
             multi_hit: None,
-            // TODO: 30% flinch
             secondary_effect: None,
-            critical_hit: false,
+            critical_hit: true,
         });
 
         result.push(Move {
@@ -84,6 +130,23 @@ lazy_static! {
             accuracy: Some(100),
             pp: 30,
             priority: 1,
+            target_type: TargetType::SingleAdjacentTarget,
+            multi_hit: None,
+            secondary_effect: None,
+            critical_hit: false,
+        });
+
+        result.push(Move {
+            id: "Pound".to_string(),
+            display_name: "Pound".to_string(),
+            description: "".to_string(), // TODO
+            move_type: PokemonType::Normal,
+            category: MoveCategory::Physical,
+            base_power: MovePower::Constant(40),
+            power_modifier: None,
+            accuracy: Some(100),
+            pp: 35,
+            priority: 0,
             target_type: TargetType::SingleAdjacentTarget,
             multi_hit: None,
             secondary_effect: None,
@@ -125,6 +188,23 @@ lazy_static! {
         });
 
         result.push(Move {
+            id: "Tackle".to_string(),
+            display_name: "Tackle".to_string(),
+            description: "".to_string(), // TODO
+            move_type: PokemonType::Normal,
+            category: MoveCategory::Physical,
+            base_power: MovePower::Constant(40),
+            power_modifier: None,
+            accuracy: Some(100),
+            pp: 35,
+            priority: 0,
+            target_type: TargetType::SingleAdjacentTarget,
+            multi_hit: None,
+            secondary_effect: None,
+            critical_hit: false,
+        });
+
+        result.push(Move {
             id: "TailWhip".to_string(),
             display_name: "Tail Whip".to_string(),
             description: "".to_string(), // TODO
@@ -145,86 +225,6 @@ lazy_static! {
                 }
             }),
             critical_hit: false,
-        });
-
-        result.push(Move {
-            id: "Harden".to_string(),
-            display_name: "Harden".to_string(),
-            description: "".to_string(), // TODO
-            move_type: PokemonType::Normal,
-            category: MoveCategory::Status,
-            base_power: MovePower::Special,
-            power_modifier: None,
-            accuracy: None,
-            pp: 30,
-            priority: 0,
-            target_type: TargetType::User,
-            multi_hit: None,
-            secondary_effect: Some(SecondaryEffect {
-                chance: 100,
-                effect: SimpleEffect::StatChange {
-                    changes: vec![(Stat::Defense, 1)],
-                    target: SimpleEffectTarget::MoveUser,
-                }
-            }),
-            critical_hit: false,
-        });
-
-        result.push(Move {
-            id: "Growl".to_string(),
-            display_name: "Growl".to_string(),
-            description: "".to_string(), // TODO
-            move_type: PokemonType::Normal,
-            category: MoveCategory::Status,
-            base_power: MovePower::Special,
-            power_modifier: None,
-            accuracy: Some(100),
-            pp: 40,
-            priority: 0,
-            target_type: TargetType::AllAdjacentFoes,
-            multi_hit: None,
-            secondary_effect: Some(SecondaryEffect {
-                chance: 100,
-                effect: SimpleEffect::StatChange {
-                    changes: vec![(Stat::Attack, -1)],
-                    target: SimpleEffectTarget::MoveTarget,
-                }
-            }),
-            critical_hit: false,
-        });
-
-        result.push(Move {
-            id: "Pound".to_string(),
-            display_name: "Pound".to_string(),
-            description: "".to_string(), // TODO
-            move_type: PokemonType::Normal,
-            category: MoveCategory::Physical,
-            base_power: MovePower::Constant(40),
-            power_modifier: None,
-            accuracy: Some(100),
-            pp: 35,
-            priority: 0,
-            target_type: TargetType::SingleAdjacentTarget,
-            multi_hit: None,
-            secondary_effect: None,
-            critical_hit: false,
-        });
-
-        result.push(Move {
-            id: "KarateChop".to_string(),
-            display_name: "Karate Chop".to_string(),
-            description: "".to_string(), // TODO
-            move_type: PokemonType::Fight,
-            category: MoveCategory::Physical,
-            base_power: MovePower::Constant(50),
-            power_modifier: None,
-            accuracy: Some(100),
-            pp: 25,
-            priority: 0,
-            target_type: TargetType::SingleAdjacentTarget,
-            multi_hit: None,
-            secondary_effect: None,
-            critical_hit: true,
         });
 
         MoveDex::new(
