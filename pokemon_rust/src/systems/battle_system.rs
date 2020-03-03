a1b32287ceb230ee8cf0d5339a3183c76aba91ca
Ghabriel <ghabriel.nunes@gmail.com>
2020-03-03 11:23:56 -0300

    Animate pokémon positioning on switch in

--- pokemon_rust/src/systems/battle_system.rs
+++ pokemon_rust/src/systems/battle_system.rs
@@ -2,8 +2,8 @@
 
 use amethyst::{
     assets::{AssetStorage, Loader},
-    core::{math::Vector3, Transform},
-    ecs::{Entities, Read, ReadExpect, System, WriteExpect, WriteStorage},
+    core::{math::Vector3, Time, Transform},
+    ecs::{Entities, Entity, Read, ReadExpect, System, WriteExpect, WriteStorage},
     renderer::{SpriteRender, SpriteSheet, Texture},
 };
 
@@ -30,27 +30,31 @@ use crate::{
 
 use std::collections::VecDeque;
 
-// TODO: move these constants somewhere else
+// TODO: move these window-related constants somewhere else
 const WINDOW_WIDTH: f32 = 800.;
 const WINDOW_HEIGHT: f32 = 600.;
 
-fn get_p1_sprite_transform() -> Transform {
-    let x = BATTLE_CAMERA_POSITION.0 - WINDOW_WIDTH / 3.;
-    let y = BATTLE_CAMERA_POSITION.0 - WINDOW_HEIGHT / 4.;
+const P1_SPRITE_Y: f32 = BATTLE_CAMERA_POSITION.0 - WINDOW_HEIGHT / 4.;
+const P2_SPRITE_Y: f32 = BATTLE_CAMERA_POSITION.0 + WINDOW_HEIGHT / 4.;
 
+// Both initial positions should be off-screen to improve the animation
+const P1_SPRITE_INITIAL_X: f32 = BATTLE_CAMERA_POSITION.0 - WINDOW_WIDTH - 100.;
+const P2_SPRITE_INITIAL_X: f32 = BATTLE_CAMERA_POSITION.0 + WINDOW_WIDTH + 100.;
+
+const P1_SPRITE_FINAL_X: f32 = BATTLE_CAMERA_POSITION.0 - WINDOW_WIDTH / 3.;
+const P2_SPRITE_FINAL_X: f32 = BATTLE_CAMERA_POSITION.0 + WINDOW_WIDTH / 3.;
+
+fn get_p1_sprite_transform() -> Transform {
     let mut transform = Transform::default();
-    transform.set_translation_xyz(x, y, 0.);
+    transform.set_translation_xyz(P1_SPRITE_INITIAL_X, P1_SPRITE_Y, 0.);
     transform.set_scale(Vector3::new(2., 2., 2.));
 
     transform
 }
 
 fn get_p2_sprite_transform() -> Transform {
-    let x = BATTLE_CAMERA_POSITION.0 + WINDOW_WIDTH / 3.;
-    let y = BATTLE_CAMERA_POSITION.0 + WINDOW_HEIGHT / 4.;
-
     let mut transform = Transform::default();
-    transform.set_translation_xyz(x, y, 0.);
+    transform.set_translation_xyz(P2_SPRITE_INITIAL_X, P2_SPRITE_Y, 0.);
     transform.set_scale(Vector3::new(1.8, 1.8, 1.8));
 
     transform
@@ -77,7 +81,8 @@ pub struct BattleSystem {
 enum Animation {
     InitialSwitchIn {
         event_data: InitialSwitchIn,
-        time: usize,
+        pokemon_entity: Entity,
+        elapsed_time: f32,
     },
     Damage {
         // TODO: keep track of HP bar animation parameters
@@ -98,13 +103,13 @@ impl BattleSystem {
         ));
     }
 
-    fn init_animation(&mut self) {
+    fn init_animation(&mut self, system_data: &mut <Self as System<'_>>::SystemData) {
         let event = self.event_queue.pop_front().unwrap();
         println!("{:?}", event);
 
         match event {
             BattleEvent::InitialSwitchIn(event_data) => {
-                self.active_animation = Some(Animation::InitialSwitchIn { event_data, time: 0 });
+                self.init_switch_in(event_data, system_data);
             },
             BattleEvent::ChangeTurn(_) => self.finish_animation(),
             BattleEvent::Damage(event_data) => {
@@ -133,6 +138,60 @@ impl BattleSystem {
             Animation::StatChange { event_data, time } => { },
         }
     }
+}
+
+impl BattleSystem {
+    fn init_switch_in(
+        &mut self,
+        event_data: InitialSwitchIn,
+        system_data: &mut <Self as System<'_>>::SystemData,
+    ) {
+        let (
+            battle,
+            sprite_renders,
+            transforms,
+            entities,
+            asset_tracker,
+            loader,
+            sprite_sheet_storage,
+            texture_storage,
+            time,
+        ) = system_data;
+
+        let (image_name, transform) = if event_data.team == Team::P1 {
+            ("pokemon/gen1_back.png", get_p1_sprite_transform())
+        } else {
+            ("pokemon/gen1_front.png", get_p2_sprite_transform())
+        };
+
+        let sprite_sheet = load_sprite_sheet(
+            &loader,
+            &texture_storage,
+            &sprite_sheet_storage,
+            image_name,
+            "pokemon/gen1_sprites.ron",
+            asset_tracker.get_progress_counter_mut(),
+        );
+
+        let pokemon_species = self.backend.as_ref().unwrap().get_species(event_data.pokemon);
+
+        let sprite_render = SpriteRender {
+            sprite_sheet,
+            sprite_number: pokemon_species.national_number - 1,
+        };
+
+        let pokemon_entity = entities
+            .build_entity()
+            .with(sprite_render, sprite_renders)
+            .with(transform, transforms)
+            .build();
+
+        self.active_animation = Some(Animation::InitialSwitchIn {
+            event_data,
+            pokemon_entity,
+            elapsed_time: 0.,
+        });
+    }
 
     fn tick_switch_in(
         &mut self,
@@ -145,40 +204,36 @@ impl BattleSystem {
             loader,
             sprite_sheet_storage,
             texture_storage,
+            time,
         ): <Self as System<'_>>::SystemData,
     ) {
         let animation = self.active_animation.as_mut().unwrap();
 
-        if let Animation::InitialSwitchIn { event_data, time } = animation {
-            let (image_name, transform) = if event_data.team == Team::P1 {
-                ("pokemon/gen1_back.png", get_p1_sprite_transform())
+        if let Animation::InitialSwitchIn { event_data, pokemon_entity, elapsed_time } = animation {
+            let mut transform = transforms
+                .get_mut(*pokemon_entity)
+                .expect("Failed to retrieve Transform");
+
+            // TODO: extract to constant
+            let total_time = 2.;
+
+            let progress = (*elapsed_time / total_time).min(1.);
+            let x = match event_data.team {
+                Team::P1 => {
+                    P1_SPRITE_INITIAL_X + (P1_SPRITE_FINAL_X - P1_SPRITE_INITIAL_X) * progress
+                },
+                Team::P2 => {
+                    P2_SPRITE_INITIAL_X + (P2_SPRITE_FINAL_X - P2_SPRITE_INITIAL_X) * progress
+                },
+            };
+
+            transform.set_translation_x(x);
+
+            if *elapsed_time >= total_time {
+                self.finish_animation();
             } else {
-                ("pokemon/gen1_front.png", get_p2_sprite_transform())
-            };
-
-            let sprite_sheet = load_sprite_sheet(
-                &loader,
-                &texture_storage,
-                &sprite_sheet_storage,
-                image_name,
-                "pokemon/gen1_sprites.ron",
-                asset_tracker.get_progress_counter_mut(),
-            );
-
-            let pokemon_species = self.backend.as_ref().unwrap().get_species(event_data.pokemon);
-
-            let sprite_render = SpriteRender {
-                sprite_sheet,
-                sprite_number: pokemon_species.national_number - 1,
-            };
-
-            entities
-                .build_entity()
-                .with(sprite_render, &mut sprite_renders)
-                .with(transform, &mut transforms)
-                .build();
-
-            self.finish_animation();
+                *elapsed_time += time.delta_seconds();
+            }
         }
     }
 }
@@ -193,14 +248,17 @@ impl<'a> System<'a> for BattleSystem {
         ReadExpect<'a, Loader>,
         Read<'a, AssetStorage<SpriteSheet>>,
         Read<'a, AssetStorage<Texture>>,
+        Read<'a, Time>,
     );
 
-    fn run(&mut self, system_data: Self::SystemData) {
-        if self.temp >= 2 {
-            return;
-        }
-
+    fn run(&mut self, mut system_data: Self::SystemData) {
         if self.active_animation.is_none() {
+            if self.temp >= 2 {
+                println!("Stopped for debugging purposes");
+                return;
+            }
+            self.temp += 1;
+
             let backend = match self.backend.as_mut() {
                 Some(backend) => backend,
                 None => {
@@ -213,10 +271,9 @@ impl<'a> System<'a> for BattleSystem {
                 self.event_queue.extend(backend.tick());
             }
 
-            self.init_animation();
+            self.init_animation(&mut system_data);
         }
 
         self.tick(system_data);
-        self.temp += 1;
     }
 }
